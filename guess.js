a012e56de85c7a48b8239bff4cf4339e331086f5
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-12 15:42:00 -0400

    Restart Game section complete.

--- guess.js
+++ guess.js
@@ -83,16 +83,27 @@ while (restartGame){
     }   else{
         guess = prompt (`Too high.  You have ${attempts} attempt(s) left.`);
     }
-
-
-
-
-
-
-        break;
     }
+//  Prompts user with option to play again
+    playAgain = prompt(`Would you like to play again? Y for yes.  N for no.`)
 
+    // Loope continues until user submits a valid response
+    while (true){
+        // Checks if the user's response is no (AKA "N" or "n")
+        if (playAgain.toUpperCase() === "N") {
+            // Alerts the useer the game is over and the game/loop does not restart
+            alert(`Thanks for playing!`);
+            restartGame = false;
+            break;
 
-    break;
+            // Checks if the user's response is yes (AKA "Y" or "y")
+        } else if(playAgain.toUpperCase() === "Y"){
+            // The game restarts
+            break;
 
+            // Prompts the user to enter a vailid response and start this loop again
+        } else {
+            playAgain = prompt(`Please enter Y or N`);
+        }
+    }
 }
